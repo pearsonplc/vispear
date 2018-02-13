1ede35559e0d6b32c941ac27fbcade068d4d2a5e
Mikolaj Bogucki <mikolaj.bogucki@pearson.com>
2018-02-13 11:07:57 +0100

    add fonts in appropriate format

--- R/load_enr_fonts.R
+++ R/load_enr_fonts.R
@@ -8,16 +8,20 @@ load_enr_fonts <- function(message = TRUE) {
   # check whether openSans is installed
   is_openSans <- any(extrafont::fonts() %in% "Open Sans")
 
-  if (is_openSans == FALSE) { download_enr_fonts() }
+  if (!is_openSans) { download_enr_fonts() }
   else {
-    if (message == TRUE) { message("All necessary fonts (including OpenSans) were installed.") }
+    if (message) { message("All necessary fonts (including OpenSans) were installed.") }
   }
 }
 
 # download fonts
 download_enr_fonts <- function(){
 
-    path <- c(file = system.file("fonts", "OpenSans/", package = 'vispear'))
+    path <- c(file = system.file("fonts", "ttf/", package = 'vispear'))
     system(paste0("cp -R ", path, " /Library/Fonts/"))
-    extrafont::font_import(prompt = F)
+    extrafont::font_import(prompt = F, pattern = "OpenSans")
+    extrafont::loadfonts(quiet = T)
+
+    # restart RStudio session
+    .rs.restartR()
 }
