da754a21e476e80ee81e250873d398f24fe05036
Mikolaj Bogucki <mikolaj.bogucki@pearson.com>
2018-02-21 12:28:47 +0100

    delete p_colors2 - create one 8-coloured palette

--- R/p_colors.R
+++ R/p_colors.R
@@ -1,6 +1,6 @@
-#' 4 different colors - Pearson colors
+#' 8 different colors - Pearson colors (second palette)
 #' @export
 
-p_colors <- c("#007FA3", "#003057", "#D2DB0E", "#D4EAE4")
+p_colors <- c("#003057", "#D2DB0E", "#12B2A6", "#505759", "#DB0020", "#9E007E", "#EA7600", "#005A70")
 
-attr(p_colors, "message") <- "4-coloured Pearson palette"
+attr(p_colors, "message") <- "8-coloured Pearson palette"
--- R/util.R
+++ R/util.R
@@ -1,6 +1,7 @@
 
 fonts_save <- function() {
   extrafont::loadfonts("pdf", quiet = TRUE)
+  extrafont::loadfonts("postscript", quiet = TRUE)
   if (.Platform$OS.type == "windows")
     extrafont::loadfonts("win", quiet = TRUE)
 }
